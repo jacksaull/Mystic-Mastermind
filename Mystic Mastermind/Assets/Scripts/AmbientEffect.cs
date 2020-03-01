37201884a2408c75f0ba383dc73a9feb2dcc5453
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-03-01 04:44:44 +0000

    Tutorial Level decorated and lighted, Title Screen
    Tutorial Level
    - Wind Section is fully built, decorated, lit and scripted
    Title Screen
    - Camera Loop Pan is complete and small room for Title Screen start is built and lit

---
+++ Mystic Mastermind/Assets/Scripts/AmbientEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientEffect : MonoBehaviour
+{
+    public AudioSource source; //Source to play ambient effect from
+    public AudioClip effect; //Effect to play
+    private bool hasPlayed = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player" && hasPlayed == false)
+        {
+            source.PlayOneShot(effect);
+            hasPlayed = true;
+        }
+    }
+}
