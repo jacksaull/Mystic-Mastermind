00e1e85bc5fffef9120722fd152a3494c99b42ea
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-03-13 00:57:17 +0000

    Earth Level Update
    Earth Level
    - Chamber built and lit
    - Traps and Pillars to be added for Puzzle and Earth Spell practise

---
+++ Mystic Mastermind/Assets/Scripts/DeathContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathContact : MonoBehaviour
+{
+    public Health player; //Player Health Script
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            player.Death();
+            Debug.Log("Dead");
+        }
+    }
+}
