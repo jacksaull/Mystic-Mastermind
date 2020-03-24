a19a6cf266c177f2769153bae58dc7d1ed7f24c8
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-03-24 22:28:38 +0000

    Tutorial Complete
    - Wind Section Complete
    - Earth Section Complete
    - Fire Section Complete
    - End Room with Portal Complete

--- Mystic Mastermind/Assets/Scripts/FireTargetManager.cs
+++ Mystic Mastermind/Assets/Scripts/FireTargetManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class FireTargetManager : MonoBehaviour
 {
     public int targetLimit;
+    public AudioSource source;
+    public AudioClip effect;
     public Animator stoneDoor;
     void Start()
     {
         stoneDoor = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -22,6 +25,7 @@ public class FireTargetManager : MonoBehaviour
 
         if (targetLimit == 0)
         {
+            source.PlayOneShot(effect, 2);
             stoneDoor.SetBool("isTriggered", true);
         }
     }
