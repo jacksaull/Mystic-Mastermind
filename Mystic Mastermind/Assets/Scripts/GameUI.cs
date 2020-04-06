ec046679f2774a7264310af68f7d9bb44fb7e41a
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-04-06 20:26:19 +0100

    Earth Section Revamp
    Earth Section:
    - Easier Section to introduce Spell before harder section
    - Harder section made more symmertical and Anchor Points on every side to give Player more options and freedom

--- Mystic Mastermind/Assets/Scripts/GameUI.cs
+++ Mystic Mastermind/Assets/Scripts/GameUI.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
+    public TextMeshProUGUI deathCount;
+    public TextMeshProUGUI timerCount;
+
+    public Health health;
+    public Timer gameTimer;
     void Start()
     {
-        
+        health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        gameTimer = GameObject.FindWithTag("Portal").GetComponent<Timer>();
     }
 
     void Update()
     {
-        
+        deathCount.text = health.deaths.ToString();
+        timerCount.text = gameTimer.timerText.text;
     }
 }
