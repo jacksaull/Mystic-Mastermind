edbc0c719073d55f773850193e5e21c6d0a7dbd9
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-04-21 21:45:56 +0100

    Light Spell + The Towers
    Light Spell
    - Spell Script coded and prefab created and being tested
    
    The Towers
    - New Level being created and designed as first proper level following Tutorial

---
+++ Mystic Mastermind/Assets/Scripts/LightOrb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOrb : MonoBehaviour
+{
+    private float speed = 500.0f; //Speed of Fireball
+    private Rigidbody m_Rigidbody; //Rigidbody of Fireball
+
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        m_Rigidbody.AddForce(m_Rigidbody.transform.forward * speed);
+        Invoke("Delete", 10);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Delete()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Mystic Mastermind/Assets/Scripts/LightSpell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpell : BaseSpell
+{
+    public GameObject lookDirection; //The Object of which the Orb will come from
+    public GameObject Orb; //Orb Object
+    void Start()
+    {
+        spellName = "Light";
+
+        spellManager = GameObject.FindWithTag("Spell Manager").GetComponent<SpellManager>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /*This method manages what occurs when the Spell is selected and Cast*/
+    public override void CastSpell(string spellSlot)
+    {
+        castSpellSlot = spellSlot;
+        player.PlayOneShot(soundEffect, 1);
+        Instantiate(Orb, lookDirection.transform.position, lookDirection.transform.rotation);
+
+        Invoke("Recharge", 5f);
+    }
+
+    void Recharge()
+    {
+        RechargeSpell(castSpellSlot);
+    }
+}
