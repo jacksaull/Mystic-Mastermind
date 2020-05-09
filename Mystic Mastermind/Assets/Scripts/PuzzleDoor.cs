efe56a42e77416b74251357f0f35f468e1e77245
jacksaull <32143271+jacksaull@users.noreply.github.com>
2020-05-09 22:01:10 +0100

    Puzzle Room
    Puzzle Room
    - Puzzles added and tested and Puzzle Door opens

---
+++ Mystic Mastermind/Assets/Scripts/PuzzleDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDoor : MonoBehaviour
+{
+    public AudioSource source;
+    public Animator animator;
+    public int correctPlacement = 0;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (correctPlacement == 4)
+        {
+            Invoke("OpenDoor", 5);
+        }
+    }
+
+    void OpenDoor()
+    {
+        animator.SetBool("isTriggered", true);
+        source.Play();
+    }
+}
---
+++ Mystic Mastermind/Assets/Scripts/SwitchTrigger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrigger : MonoBehaviour
+{
+    public GameObject prompt;
+    bool inRange;
+    bool cooldown;
+    bool inPlace;
+
+    public PuzzleDoor puzzleDoor;
+    public GameObject puzzleDoorLayer;
+    public int puzzleDoorLayerNum;
+    void Start()
+    {
+        inRange = false;
+        cooldown = false;
+        inPlace = false;
+        puzzleDoor = GameObject.FindWithTag("Puzzle Door").GetComponent<PuzzleDoor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("t") && cooldown == false)
+        {
+            if (inRange == true)
+            {
+                puzzleDoor.source.Play();
+                cooldown = true;
+                Invoke("Cooldown", 5);
+
+                puzzleDoorLayer.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
+
+                puzzleDoorLayerNum++;
+                if (puzzleDoorLayerNum == 4)
+                {
+                    puzzleDoorLayerNum = 0;
+                }
+            }
+            if (puzzleDoorLayerNum == 0)
+            {
+                inPlace = true;
+                puzzleDoor.correctPlacement++;
+            }
+            else if (puzzleDoorLayerNum != 0 && inPlace == true)
+            {
+                puzzleDoor.correctPlacement--;
+                inPlace = false;
+            }
+        }
+    }
+
+    void Cooldown()
+    {
+        cooldown = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            prompt.SetActive(true);
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            prompt.SetActive(false);
+            inRange = false;
+        }
+    }
+}
